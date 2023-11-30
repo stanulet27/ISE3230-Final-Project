919ad8d4bb04c9f3ab6c67b2915567f28b2ea89f
chris <stanulet.1@osu.edu>
2023-11-29 23:03:27 -0500

    working code

--- Project.py
+++ Project.py
@@ -2,39 +2,51 @@ import cvxpy as cp
 import numpy as np
 import csv
 
+#the total expected demand for these vehicles
+totalExpectedSales = 696067
+
+#emissions per car
 emissions = []
+#profits per car
 profits = []
+# number of units sold in 2022 per car
+sales = []
+
 with open('HondaEmissions.csv', newline='') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
         emissions.append(row['g/mi'])
         profits.append(row['Estimated Profit'])
 
-sales = []
-
 with open('HondaSales.csv', newline='') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
-        emissions.append(row['units sold in 2022'])
+        sales.append(row['min cars'])
 
 
 x = cp.Variable(len(emissions), integer = True)
 
-#obj_func = profits[0]*x[0]+profits[1]*x[1]+profits[2]*x[2]+profits[3]*x[3]+profits[4]*x[4]+profits[5]*x[5]+profits[6]*x[6]+profits[7]*x[7]+profits[8]*x[8]+profits[9]*x[9]+profits[10]*x[10]+profits[11]*x[11]+profits[12]*x[12]+profits[13]*x[13]+profits[14]*x[14]
+
 obj_func = cp.sum([profits[i]*x[i] for i in range(len(profits))])
-print(obj_func)
+
 constraints = []
 
-
 #Adding the emission constraint
-constraints.append(np.multiply(np.array(emissions).astype(float) - 307.0,x) <= 307)
-#constraints.append(cp.sum([(emissions[i]-307)*x[i] for i in range(len(emissions))]) <= 307)
+constraints.append(np.multiply(np.array(emissions).astype(float) - 307.0,x) <= 0)
+
 #Adding the sales constraint
+constraints.append(x[0] + x[1] + x[2] + x[3] >= sales[0]) #all civics
+constraints.append(x[4] + x[5] >= sales[1])#all accords
 
+#all other cars are single
+for i in range(len(sales) -2):
+    constraints.append(x[i+6] >= sales[i+2])
 
+# bound total sales to expected number
+constraints.append(cp.sum([x[i] for i in range(len(profits))]) <= totalExpectedSales)
 
 #Adding non-negative constraints
-for i in range(15):   
+for i in range(len(emissions)):   
     constraints.append(x[i] >= 0)
 
             
