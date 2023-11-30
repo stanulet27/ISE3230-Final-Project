cf7e14650e9f1cbbc51ae7217df1388231257089
chris <stanulet.1@osu.edu>
2023-11-29 19:31:43 -0500

    add max growth constraint

--- Project.py
+++ Project.py
@@ -3,14 +3,16 @@ import numpy as np
 import csv
 
 #the total expected demand for these vehicles
-totalExpectedSales = 696067
+totalExpectedSales = 969067
 
 #emissions per car
 emissions = []
 #profits per car
 profits = []
-# number of units sold in 2022 per car
-sales = []
+# 90% of units sold in 2022
+minSales = []
+# 200% of units sold in 2022
+maxSales = []
 
 with open('HondaEmissions.csv', newline='') as csvfile:
     reader = csv.DictReader(csvfile)
@@ -21,7 +23,8 @@ with open('HondaEmissions.csv', newline='') as csvfile:
 with open('HondaSales.csv', newline='') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
-        sales.append(row['min cars'])
+        minSales.append(row['min cars'])
+        maxSales.append(row['max cars'])
 
 
 x = cp.Variable(len(emissions), integer = True)
@@ -32,18 +35,26 @@ obj_func = cp.sum([profits[i]*x[i] for i in range(len(profits))])
 constraints = []
 
 #Adding the emission constraint
-constraints.append(np.multiply(np.array(emissions).astype(float) - (307.0* .95),x) <= 0)
+constraints.append(np.multiply(np.array(emissions).astype(float) - (307.0 * .925),x) <= 0)
 
-#Adding the sales constraint
-constraints.append(x[0] + x[1] + x[2] + x[3] >= sales[0]) #all civics
-constraints.append(x[4] + x[5] >= sales[1])#all accords
+#Adding the minSales constraint
+constraints.append(x[0] + x[1] + x[2] + x[3] >= minSales[0]) #all civics
+constraints.append(x[4] + x[5] >= minSales[1])#all accords
 
 #all other cars are single
-for i in range(len(sales) -2):
-    constraints.append(x[i+6] >= sales[i+2])
+for i in range(len(minSales) -2):
+    constraints.append(x[i+6] >= minSales[i+2])
 
-# bound total sales to expected number
+# bound total minSales to expected number
 constraints.append(cp.sum([x[i] for i in range(len(profits))]) <= totalExpectedSales)
+
+#add constraint disallowing more than 2x in sales
+constraints.append(x[0] + x[1] + x[2] + x[3] <=  maxSales[0]) #all civics
+constraints.append(x[4] + x[5] <=  maxSales[1])#all accords
+
+#all other cars are single
+for i in range(len(maxSales) -2):
+    constraints.append(x[i+6] <= maxSales[i+2])
 
 #Adding non-negative constraints
 for i in range(len(emissions)):   
